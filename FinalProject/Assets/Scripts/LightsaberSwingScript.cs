96114f27d8717222e33d5dc128a0a1de365dcf6e
ssanti15 <ssanti15@asu.edu>
2024-04-03 20:13:48 -0700

    Lightsaber Throw added
    Added a lightsaber throw ability (bound to right click)

--- FinalProject/Assets/Scripts/LightsaberSwingScript.cs
+++ FinalProject/Assets/Scripts/LightsaberSwingScript.cs
@@ -8,8 +8,10 @@ public class LightsaberSwingScript : MonoBehaviour
     public GameObject Lightsaber;
 
     //Add a cooldown so the player cannot spam attack and bug the animation
-    public float cooldownTime = 1f;
-    private float lastUsedTime;
+    public float SwingcooldownTime = 1f;
+    public float ThrowcooldownTime = 5f;
+    private float SwinglastUsedTime;
+    private float ThrowlastUsedTime;
 
     
 
@@ -24,20 +26,41 @@ public class LightsaberSwingScript : MonoBehaviour
     void Update()
     {
         //If left mouse button is clicked then start the animation
-        if(Input.GetMouseButtonDown(0) && Time.time > lastUsedTime + cooldownTime)
+        if(Input.GetMouseButtonDown(0) && Time.time > SwinglastUsedTime + SwingcooldownTime)
         {
             //enable lightsaber hitbox when attacking
             GetComponent<Collider>().enabled = true;
             //animation
             StartCoroutine(LightsaberSwing());
             //update cooldown
-            lastUsedTime = Time.time;
+            SwinglastUsedTime = Time.time;
+        }
+
+        if (Input.GetMouseButtonDown(1) && Time.time > ThrowlastUsedTime + ThrowcooldownTime)
+        {
+            //enable lightsaber hitbox when attacking
+            GetComponent<Collider>().enabled = true;
+            //animation
+            StartCoroutine(LightsaberThrow());
+            //update cooldown
+            ThrowlastUsedTime = Time.time;
+            Debug.Log("Throw");
         }
     }
 
     IEnumerator LightsaberSwing()
     {
         Lightsaber.GetComponent<Animator>().Play("LightsaberSwing");
+        yield return new WaitForSeconds(1.0f);
+        Lightsaber.GetComponent<Animator>().Play("New State");
+
+        //turn off lightsaber hitbox when the animation is over
+        GetComponent<Collider>().enabled = false;
+    }
+
+    IEnumerator LightsaberThrow()
+    {
+        Lightsaber.GetComponent<Animator>().Play("Lightsaber Throw");
         yield return new WaitForSeconds(1.0f);
         Lightsaber.GetComponent<Animator>().Play("New State");
 
